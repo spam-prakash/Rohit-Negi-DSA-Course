c76ddb21a3bcca78ccd2a4640130230b35aa289f
Prakash Kumar <dev.prakash.spam@gmail.com>
2026-01-08 17:03:54 +0530

    Day 70: Generate Subse

---
+++ Revise Lecture 61 Recrusion on Subsequence/generate_paranthesis.cpp
@@ -0,0 +1,33 @@
+
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+void generate(vector<string> &ans,int n,int l,int r,string &temp){
+    if(l==n && r==n){
+        ans.push_back(temp);
+        return;
+    }
+
+    if(l<n){
+        temp.push_back('(');
+        generate(ans,n,l+1,r,temp);
+        temp.pop_back();
+    }
+    if(r<l){
+        temp.push_back(')');
+        generate(ans,n,l,r+1,temp);
+        temp.pop_back();
+    }
+}
+
+int main(){
+    int n=4;
+    vector<string> ans;
+    string s;
+    generate(ans,n,0,0,s);
+     for (int i = 0; i < ans.size(); i++) {
+        cout << i+1<<" "<<ans[i] << endl;
+    }
+}
---
+++ Revise Lecture 61 Recrusion on Subsequence/print_subsequence.cpp
@@ -0,0 +1,31 @@
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void subseq(int arr[],int index,int n,vector<vector<int>> &ans, vector<int> &temp){
+    if(index==n) {
+        ans.push_back(temp);
+        return;
+    }
+    subseq(arr,index+1,n,ans,temp);
+    temp.push_back(arr[index]);
+    subseq(arr,index+1,n,ans,temp);
+    temp.pop_back();
+}
+
+int main(){
+    int arr[3]={1,2,3};
+    vector<vector<int>> ans;
+    vector<int> temp;
+    subseq(arr,0,3,ans,temp);
+
+    for(int i=0;i<ans.size();i++){
+        cout<<"{ ";
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<"}";
+        cout<<endl;
+    }
+}
