f09bdb43fafa63a0495310395c8f2fddefca17f4
Prakash Kumar <dev.prakash.spam@gmail.com>
2026-01-09 18:23:03 +0530

    Day 71: Perfect Sum, Repeated Sum

--- Lecture 63 Recursion Perfect Sum Problem  Target Sum with Repetition/repeat_sum.cpp
+++ Lecture 63 Recursion Perfect Sum Problem  Target Sum with Repetition/repeat_sum.cpp
@@ -3,12 +3,8 @@
 using namespace std;
 
 int sum(int arr[],int index,int n,int target){
-   if(target==0){
-    return 1;
-   }
-   if(index==n || target<0){
-    return 0;
-   }
+   if(target==0) return 1;
+   if(index==n || target<0) return 0;
 
     return sum(arr,index+1,n,target)+sum(arr,index,n,target-arr[index]);
 }
