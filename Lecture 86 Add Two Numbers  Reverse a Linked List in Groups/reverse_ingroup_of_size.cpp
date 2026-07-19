2c0732da77b0f315e3db8813e0e18e6a37ff8684
Prakash Kumar <dev.prakash.spam@gmail.com>
2026-07-19 16:50:00 +0530

    Day 94: Reverse a Linked List inGroup of Sizes

--- Lecture 86 Add Two Numbers  Reverse a Linked List in Groups/reverse_ingroup_of_size.cpp
+++ Lecture 86 Add Two Numbers  Reverse a Linked List in Groups/reverse_ingroup_of_size.cpp
@@ -28,7 +28,41 @@ void displayLinkedList(Node *dis){
 
 class Solution{
     public:
+       Node* reverse(Node* head, int k) {
+            Node dummy(0);
+            dummy.next = head;
 
+            Node* groupPrev = &dummy;
+
+            while (true) {
+
+                Node* kth = groupPrev;
+
+                for (int i = 0; i < k && kth; i++)
+                    kth = kth->next;
+
+                if (!kth)
+                    break;
+
+                Node* groupNext = kth->next;
+
+                Node* prev = groupNext;
+                Node* curr = groupPrev->next;
+
+                while (curr != groupNext) {
+                    Node* temp = curr->next;
+                    curr->next = prev;
+                    prev = curr;
+                    curr = temp;
+                }
+
+                Node* temp = groupPrev->next;
+                groupPrev->next = kth;
+                groupPrev = temp;
+            }
+
+            return dummy.next;
+        }
 };
 
 int main(){
@@ -39,5 +73,7 @@ int main(){
     displayLinkedList(head);
 
     Solution obj;
+    head=obj.reverse(head,size);
+    displayLinkedList(head);
 
 }
